3ea3c0a0a0ba0450aca3d8b690cdca0259728a8e
Ryan Pereira <33844142+rcovery@users.noreply.github.com>
2022-07-11 21:39:47 -0300

    Update ultrasonic.ino

--- ultrasonic/ultrasonic.ino
+++ ultrasonic/ultrasonic.ino
@@ -1,7 +1,7 @@
 const int pinT = 3; // Pino Digital Triq (ping)
 const int pinE = 2; // Pino Digital Echo (retorno)
 const int pinLed = 13; // Led pra diferenciar
-long duration, cent; // Microsegundos, centímetros
+long duration, centimeters; // Microsegundos, centímetros
 
 void setup() {
   pinMode(pinT, OUTPUT);
@@ -19,11 +19,10 @@ void loop() {
   digitalWrite(pinT, LOW);
 
   duration = pulseIn(pinE, HIGH);
-  cent = (duration / 2) / 80; // Transformar os microsegundos em centímetros
+  centimeters = (duration / 2) / 80; // Transformar os microsegundos em centímetros
   
-  Serial.println((String)"Distância: " + cent + "cm");
+  Serial.println((String)"Distância: " + centimeters + "cm");
   
-  digitalWrite(pinLed, cent < 15 ? HIGH : LOW);
-
+  digitalWrite(pinLed, centimeters < 15 ? HIGH : LOW);
   delay(300);
 }
